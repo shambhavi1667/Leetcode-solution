26dd0520b00768417eef42de0f69d2c76819d0c4
shambhavi kulkarni <shambhaviik18@gmail.com>
2026-02-19 23:48:07 +0530

    subarray sum divisible by k

---
+++ arrays/subarraysumdivisiblebyk.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        int prefixSum = 0;
+        int count = 0;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        for (int num : nums) {
+            prefixSum += num;
+
+            int remainder = prefixSum % k;
+
+            if (remainder < 0) {
+                remainder += k;
+            }
+
+            if (map.containsKey(remainder)) {
+                count += map.get(remainder);
+            }
+
+            map.put(remainder, map.getOrDefault(remainder, 0) + 1);
+        }
+
+        return count;
+    }
+}
