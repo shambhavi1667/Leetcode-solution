12ef27b74512d40843025ba375d6b9c56a113c21
shambhavi kulkarni <shambhaviik18@gmail.com>
2026-02-19 23:46:42 +0530

    find pivot index leetcode solution

---
+++ arrays/findpivotindex.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        
+        int totalSum = 0;
+        
+        // Step 1: Find total sum of array
+        for (int num : nums) {
+            totalSum += num;
+        }
+        
+        int leftSum = 0;
+        
+        // Step 2: Traverse and check pivot condition
+        for (int i = 0; i < nums.length; i++) {
+            
+            int rightSum = totalSum - leftSum - nums[i];
+            
+            if (leftSum == rightSum) {
+                return i;   // return leftmost pivot
+            }
+            
+            leftSum += nums[i];
+        }
+        
+        return -1;  // no pivot found
+    }
+}
